359b3d316a4f3684633a59a3c88cb35095b6e73b
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-14 11:59:23 +0200

    clean

--- docker/2-test/2-test-1-api/docker.build.sh
+++ docker/2-test/2-test-1-api/docker.build.sh
@@ -6,5 +6,6 @@ cp ../2-test-0-storage/storage_id_rsa* .
 
 cat Dockerfile.in | envsubst > Dockerfile
 sudo docker build --no-cache -t quicksave/test-api .
-rm Dockerfile
 
+rm Dockerfile
+rm storage_id_rsa*
