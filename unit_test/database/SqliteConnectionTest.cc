f4e98a85979027ce1b2c22f929dc05f4d740e477
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-14 08:03:15 +0200

    test

---
+++ unit_test/database/SqliteConnectionTest.cc
@@ -0,0 +1,18 @@
+// This file is a part of quicksave project.
+// Copyright (c) 2017 Aleksander Gajewski <adiog@quicksave.io>.
+
+#include <gtest/gtest.h>
+
+#include <qs/database/SqliteConnection.h>
+
+
+TEST(SqliteConnectionTestSuite, Constructor)
+{
+    auto sqliteConnection = SqliteConnection("/storage/users/testuser.sqlite");
+}
+
+TEST(SqliteConnectionTestSuite, GettingTransaction)
+{
+    auto sqliteConnection = SqliteConnection("/storage/users/testuser.sqlite");
+    auto transaction = sqliteConnection.getTransaction();
+}
