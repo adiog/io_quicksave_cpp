f397a0053703efe23544560174f425fc5d80622b
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-24 15:52:10 +0200

    fix docker build

--- docker/0-dev/0-dev-5-curl/docker.build.sh
+++ docker/0-dev/0-dev-5-curl/docker.build.sh
@@ -2,6 +2,8 @@
 # This file is a part of quicksave project.
 # Copyright 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
+cd $(dirname $0)
+
 wget -O qs-audit https://raw.githubusercontent.com/adiog/io_quicksave_audit-server/master/cli/qs-audit
 chmod +x qs-audit
 
--- docker/2-test/2-test-2-cdn/docker.build.sh
+++ docker/2-test/2-test-2-cdn/docker.build.sh
@@ -2,6 +2,8 @@
 # This file is a part of quicksave project.
 # Copyright 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
+cd $(dirname $0)
+
 cp ../2-test-0-storage/storage_id_rsa* .
 
 cat Dockerfile.in | envsubst > Dockerfile
