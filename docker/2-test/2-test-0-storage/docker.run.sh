1435d19c901497238e1afad7bd5e9574a999609b
Aleksander Gajewski <adiog@brainfuck.pl>
2019-02-05 07:46:46 +0100

    crappy-fixing test containers

--- docker/2-test/2-test-0-storage/docker.run.sh
+++ docker/2-test/2-test-0-storage/docker.run.sh
@@ -2,8 +2,8 @@
 # This file is a part of quicksave project.
 # Copyright 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
-sudo docker rm storage_quicksave_docker
-sudo docker run \
+docker rm storage_quicksave_docker
+docker run \
     --privileged \
     --name storage_quicksave_docker \
     --net network_quicksave_docker \
